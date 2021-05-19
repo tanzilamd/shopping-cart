02729abcc3d94a2ac233cc743c44dfb6155d7a98
tanzilamd <mdtanzil858@gmail.com>
2021-05-19 14:31:18 +0600

    phone case increase button function added with price

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-document.getElementById('case-increase');
+document.getElementById('case-increase').addEventListener('click', function(){
+    const caseInput = document.getElementById('case-count');
+    const caseCount = parseInt(caseInput.value);
+    const caseNewCount = caseCount + 1;
+    caseInput.value = caseNewCount;
+    
+    const caseTotal = caseNewCount * 59;
+    document.getElementById('case-total').innerText = '$' + caseTotal;
+});
