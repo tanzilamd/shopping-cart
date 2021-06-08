69cca216acc8071b78b92e3ec2609673a4db93ca
tanzilamd <mdtanzil858@gmail.com>
2021-06-08 18:53:03 +0600

    2nd product's increase and decrease function added

--- script.js
+++ script.js
@@ -1,20 +1,46 @@
 // Case Increase button event handler
-document.getElementById('case-increase').addEventListener('click', function(){
-    const caseInput = document.getElementById('case-count');
-    const caseCount = parseInt(caseInput.value);
-    const caseNewCount = caseCount + 1;
-    caseInput.value = caseNewCount;
-    
-    const caseTotal = caseNewCount * 59;
-    document.getElementById('case-total').innerText = '$' + caseTotal;
-});
+// document.getElementById('case-increase').addEventListener('click', function(){
+//     handleProductChange(true);
+// });
 
 // Case Decrease button event handler
-document.getElementById('case-decrease').addEventListener('click', function(){
+// document.getElementById('case-decrease').addEventListener('click', function(){
+//     handleProductChange(false);
+// });
+// Increase and decrease function
+function handleProductChange (isIncrease) {
     const caseInput = document.getElementById('case-count');
     const caseCount = parseInt(caseInput.value);
-    const caseNewCount = caseCount - 1;
+    let caseNewCount = caseCount;
+    if (isIncrease == true) {
+        caseNewCount = caseCount + 1;
+    }
+    if (isIncrease == false && caseCount > 0) {
+        caseNewCount = caseCount - 1;
+    }
+
     caseInput.value = caseNewCount;
     const caseTotal = caseNewCount * 59;
-    document.getElementById('case-total').innerText = caseTotal;
-});
+    document.getElementById('case-total').innerText = '$' + caseTotal;
+}
+
+// // Case Increase button event handler
+// document.getElementById('case-increase').addEventListener('click', function(){
+//     const caseInput = document.getElementById('case-count');
+//     const caseCount = parseInt(caseInput.value);
+//     const caseNewCount = caseCount + 1;
+//     caseInput.value = caseNewCount;
+    
+//     const caseTotal = caseNewCount * 59;
+//     document.getElementById('case-total').innerText = '$' + caseTotal;
+// });
+
+// // Case Decrease button event handler
+// document.getElementById('case-decrease').addEventListener('click', function(){
+//     const caseInput = document.getElementById('case-count');
+//     const caseCount = parseInt(caseInput.value);
+//     const caseNewCount = caseCount - 1;
+//     caseInput.value = caseNewCount;
+//     const caseTotal = caseNewCount * 59;
+//     document.getElementById('case-total').innerText = '$' + caseTotal;
+// });
