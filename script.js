9f8c00084081bff93ad6d52e3dc88b816680a7a9
tanzilamd <mdtanzil858@gmail.com>
2021-06-09 12:30:49 +0600

    phone increase and decrease function added

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+/*------------------------------------------------------Phone change function----------------------------------------------------------------------------*/
+
+function handlePhoneChange (isIncrease) {
+    const phoneInput = document.getElementById('phone-count');
+    const phoneCount = parseInt(phoneInput.value);
+    let phoneNewCount = phoneCount;
+    if (isIncrease == true) {
+        phoneNewCount = phoneCount + 1;
+    }
+    if (isIncrease == false && phoneCount > 0) {
+        phoneNewCount = phoneCount - 1;
+    }
+
+    phoneInput.value = phoneNewCount;
+    const phoneTotal = phoneNewCount * 1219;
+    document.getElementById('phone-total').innerText = '$' + phoneTotal;
+}
+
+
+/*-------------------------------------------------------Case change function--------------------------------------------------------------------------*/
 // Case Increase button event handler
 // document.getElementById('case-increase').addEventListener('click', function(){
 //     handleProductChange(true);
